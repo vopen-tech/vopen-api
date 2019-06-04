3fbd5c6b1c5924f25a2c76b38ee402fd07b3db38
Nano Vazquez <marianodvazquez@gmail.com>
2019-06-04 17:39:50 -0300

    Set up basic data

---
+++ Models/Edition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace vopen_api.Models
+{
+    public class Edition
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace vopen_api.Repositories
+{
+    public interface IRepository<T>
+    {
+        T GetById(String id);
+
+        ICollection<T> GetAll();
+
+        void Create(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+
+    }
+}
