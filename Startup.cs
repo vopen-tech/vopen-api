39cffe99104ada0639c66137f5b239be17bffb4f
Nano Vazquez <marianodvazquez@gmail.com>
2019-06-04 12:51:37 -0300

    Initial commit with Swagger

---
+++ Startup.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace vopen_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Add Swagger generation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "vOpen API",
+                    Version = "v1",
+                    Description = "The API of the open source conferences",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Mariano Vazquez",
+                        Email = "marianodvazquez@gmail.com",
+                        Url = "https://nanovazquez.dev"
+                    },
+                    License = new License { Name = "MIT" }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            // Setup Static files server
+            // (see https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files?view=aspnetcore-2.2)
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                RequestPath = "/static-files"
+            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            // {documentname} is the c.SwaggerDoc name ("v1", see services.AddSwaggerGen)
+            app.UseSwagger(c => {
+                c.RouteTemplate = "api/{documentname}/swagger/swagger.json";
+            });
+
+            // Enable middleware to serve the Swagger UI, specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/api/v1/swagger/swagger.json", "vOpen API V1");
+                c.RoutePrefix = "api/swagger";
+            });
+        }
+    }
+}
