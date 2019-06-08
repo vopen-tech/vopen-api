6326f61b7b6cf9ab7c027724d88ac779d1334f65
Nano Vazquez <marianodvazquez@gmail.com>
2019-06-08 15:29:30 -0300

    Update edition object

--- Models/Edition.cs
+++ Models/Edition.cs
@@ -7,5 +7,21 @@ namespace vopen_api.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Location Location { get; set; }
+        public string Date { get; set; }
+        public TicketsInfo TicketsInfo { get; set; }
+    }
+
+    public class Location
+    {
+        public string FullAddress { get; set; }
+        public string VenueName { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class TicketsInfo
+    {
+        public string TicketsType { get; set; }
+        public bool IsTicketSaleOpen { get; set; }
     }
 }
