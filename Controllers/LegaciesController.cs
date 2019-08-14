8a61302e17a16894b0a9af0a1c890406a13893b6
Fernando Sonego <fsonego@algeiba.com>
2019-08-13 21:04:08 -0300

    Add Support Legacy EventBrite

--- Controllers/LegaciesController.cs
+++ Controllers/LegaciesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -182,13 +184,70 @@ namespace vopen_api.Controllers
 
         //eventbrite
         [HttpPost("IsValidAttendee")]
-        public string IsValidAttendee(LegacyApiCredentialsDTO dto)
+        public async Task<IActionResult> IsValidAttendee(LegacyIsValidAttendeeDTO dto)
         {
             this.ValidateRequest(dto);
 
-            var edition = Convert.ToInt32(this.RouteData.Values["edition"].ToString());
+            var configurationEventBrite = configuration.GetSection("EventBrite");
+            var eventBriteId = configurationEventBrite.GetSection("EventbriteEventId").Value;
+            var eventBriteUSer = configurationEventBrite.GetSection("User").Value;
+            var eventBriteKey = configurationEventBrite.GetSection("Key").Value;
+            var eventBriteClientSecret = configurationEventBrite.GetSection("ClientSecret").Value;
+            var eventBriteOAuthTokenPersonal = configurationEventBrite.GetSection("OAuthTokenPersonal").Value;
+            var eventBriteOAuthTokenAnonymous = configurationEventBrite.GetSection("OAuthTokenAnonymous").Value;
+            var eventBriteAttendeesAddress = configurationEventBrite.GetSection("AttendeesAddress").Value + 
+                                                configurationEventBrite + "/attendees/?token=" + eventBriteOAuthTokenPersonal;
+            var eventbriteTicketNamesAllowedInRaffle = configuration.GetSection("EventbriteTicketNamesAllowedInRaffle").Value;
 
-            throw new NotImplementedException();
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+           
+            /// Esto no se usa siempre llega null
+            //if (pageContinuation != null)
+            //{
+            //    attendeesAddress += "&continuation=" + pageContinuation;
+            //}
+
+            
+            string attendeesRawResponse;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(eventBriteAttendeesAddress);
+            request.Accept = "application/json";
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        attendeesRawResponse = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            var attendeesResponse = Newtonsoft.Json.JsonConvert
+              .DeserializeObject<LegacyEventbriteAttendeesResponse>(attendeesRawResponse);
+
+            LegacyEventbriteAttendeesResponse.Attendee attendee = attendeesResponse.attendees
+                .FirstOrDefault(a => a.profile.email.ToLowerInvariant() == dto.Email.ToLowerInvariant());
+
+            //NO se usa paginación
+            //if (attendee == null && attendeesResponse.pagination.has_more_items)
+            //{
+            //    attendee = await AttendeeFacade.GetEventbriteAttendee(eventbriteEventId, email,
+            //        attendeesResponse.pagination.continuation);
+            //}
+
+            if (attendeesResponse != null)
+            {
+                if (eventbriteTicketNamesAllowedInRaffle.ToLower().Contains(
+                    attendee.ticket_class_name.ToLower()) == false)
+                {
+                    attendee = null; // Found but ticket not allowed.
+                }
+            }
+
+            return Ok(attendee);
         }
 
     }
---
+++ Models/legacy/LegacyIsValidAttendeeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vopen_api.Models.legacy
+{
+    public class LegacyIsValidAttendeeDTO: LegacyApiCredentialsDTO
+    {
+        public string Email { get; set; }
+    }
+}
