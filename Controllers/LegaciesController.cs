af7cad3885f746271321727d8f3eac595b479f74
Nano Vazquez <marianodvazquez@gmail.com>
2019-08-15 22:50:08 -0300

    Update LegaciesController
    - Removed inMemory cache (for now)
    - Refactored the names a little bit
    - Refactored the ValidateRequest method (now returns a boolean
    - Added a try/catch to capture errors in Eventbrite API
    - Fixed a bug when setting up the eventBriteAttendeesAddress

--- Controllers/LegaciesController.cs
+++ Controllers/LegaciesController.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using vopen_api.Models.legacy;
 using vopen_api.Repositories;
+using static vopen_api.Models.legacy.LegacyEventbriteAttendeesResponse;
 
 namespace vopen_api.Controllers
 {
@@ -17,33 +17,17 @@ namespace vopen_api.Controllers
     [ApiController]
     public class LegaciesController : ControllerBase
     {
-
         private readonly EditionsRepository editionsRepository;
         private readonly IConfiguration configuration;
-        private IMemoryCache _cache;
-        private readonly string country;
-        private readonly string edition;
+        private readonly string eventbriteEdition;
+        private readonly string MOBILE_APP_USER = "app";
+        private readonly string MOBILE_APP_TOKEN = "66197FD1-6C77-4D20-A10D-D27BF2B7D053";
 
-        public LegaciesController(EditionsRepository editionsRepository, IConfiguration configuration, IMemoryCache memoryCache)
+        public LegaciesController(EditionsRepository editionsRepository, IConfiguration configuration)
         {
             this.editionsRepository = editionsRepository;
             this.configuration = configuration;
-            this._cache = memoryCache;
-            this.country = configuration.GetSection("Country").Value;
-            this.edition = configuration.GetSection("Edition").Value;
-        }
-
-        private void ValidateRequest(LegacyApiCredentialsDTO dto)
-        {
-            LegaciesController.ValidateAppRequest(dto.User, dto.Token);
-        }
-
-        internal static void ValidateAppRequest(string user, string token)
-        {
-            if (user.ToLower() != "app" || token.ToUpper() != "66197FD1-6C77-4D20-A10D-D27BF2B7D053")
-            {
-                throw new ApplicationException("Invalid credentials");
-            }
+            this.eventbriteEdition = configuration.GetSection("Edition").Value; // e.g vopen-ar-2019
         }
 
         //devuelve modelo LegacySpronsor
@@ -73,82 +57,71 @@ namespace vopen_api.Controllers
             throw new NotImplementedException();
         }
 
-        //eventbrite
+        // Eventbrite endpoint 
         [HttpPost("IsValidAttendee")]
-        public async Task<IActionResult> IsValidAttendee(LegacyIsValidAttendeeDTO dto)
+        public async Task<IActionResult> IsValidAttendee(LegacyIsValidAttendeeDTO attendeeInfo)
         {
-            this.ValidateRequest(dto);
-            var _cacheName = this.edition + "-" + dto.Email;
-            LegacyEventbriteAttendeesResponse.Attendee result;
-
-            if (!_cache.TryGetValue(_cacheName, out result))
+            if (!this.IsValidRequest(attendeeInfo))
             {
-
-                var configurationEventBrite = configuration.GetSection("EventBrite");
-                var eventBriteId = configurationEventBrite.GetSection("EventbriteEventId").Value;
-                var eventBriteUSer = configurationEventBrite.GetSection("User").Value;
-                var eventBriteKey = configurationEventBrite.GetSection("Key").Value;
-                var eventBriteClientSecret = configurationEventBrite.GetSection("ClientSecret").Value;
-                var eventBriteOAuthTokenPersonal = configurationEventBrite.GetSection("OAuthTokenPersonal").Value;
-                var eventBriteOAuthTokenAnonymous = configurationEventBrite.GetSection("OAuthTokenAnonymous").Value;
-                var eventBriteAttendeesAddress = configurationEventBrite.GetSection("AttendeesAddress").Value +
-                                                    configurationEventBrite + "/attendees/?token=" + eventBriteOAuthTokenPersonal;
-                var eventbriteTicketNamesAllowedInRaffle = configuration.GetSection("EventbriteTicketNamesAllowedInRaffle").Value;
-
-
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-
-
-                /// Esto no se usa siempre llega null
-                //if (pageContinuation != null)
-                //{
-                //    attendeesAddress += "&continuation=" + pageContinuation;
-                //}
-
-
-                string attendeesRawResponse;
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(eventBriteAttendeesAddress);
-                request.Accept = "application/json";
-                using (WebResponse response = await request.GetResponseAsync())
-                {
-                    using (Stream dataStream = response.GetResponseStream())
-                    {
-                        using (StreamReader reader = new StreamReader(dataStream))
-                        {
-                            attendeesRawResponse = reader.ReadToEnd();
-                        }
-                    }
-                }
-
-                var attendeesResponse = Newtonsoft.Json.JsonConvert
-                  .DeserializeObject<LegacyEventbriteAttendeesResponse>(attendeesRawResponse);
-
-                result = attendeesResponse.attendees
-                    .FirstOrDefault(a => a.profile.email.ToLowerInvariant() == dto.Email.ToLowerInvariant());
-
-                //NO se usa paginación
-                //if (attendee == null && attendeesResponse.pagination.has_more_items)
-                //{
-                //    attendee = await AttendeeFacade.GetEventbriteAttendee(eventbriteEventId, email,
-                //        attendeesResponse.pagination.continuation);
-                //}
-
-                if (attendeesResponse != null)
-                {
-                    if (eventbriteTicketNamesAllowedInRaffle.ToLower().Contains(
-                        result.ticket_class_name.ToLower()) == false)
-                    {
-                        result = null; // Found but ticket not allowed.
-                    }
-                }
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(300));
-                _cache.Set(_cacheName, result, cacheEntryOptions);
+                return Unauthorized();
             }
 
-            return Ok(result);
+            Attendee result = null;
+
+            var configurationEventBrite = configuration.GetSection("EventBrite");
+            var eventBriteId = configurationEventBrite.GetSection("EventbriteEventId").Value;
+            var eventBriteUSer = configurationEventBrite.GetSection("User").Value;
+            var eventBriteKey = configurationEventBrite.GetSection("Key").Value;
+            var eventBriteClientSecret = configurationEventBrite.GetSection("ClientSecret").Value;
+            var eventBriteOAuthTokenPersonal = configurationEventBrite.GetSection("OAuthTokenPersonal").Value;
+            var eventBriteOAuthTokenAnonymous = configurationEventBrite.GetSection("OAuthTokenAnonymous").Value;
+            var eventBriteAttendeesAddress = configurationEventBrite.GetSection("AttendeesAddress").Value + "attendees/?token=" + eventBriteOAuthTokenPersonal;
+            var eventbriteTicketNamesAllowedInRaffle = configurationEventBrite.GetSection("EventbriteTicketNamesAllowedInRaffle").Value;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            string attendeesRawResponse = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(eventBriteAttendeesAddress);
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = await request.GetResponseAsync())
+                using (Stream dataStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    attendeesRawResponse = reader.ReadToEnd();
+                }
+
+                var attendeesResponse = JsonConvert.DeserializeObject<LegacyEventbriteAttendeesResponse>(attendeesRawResponse);
+
+                if (attendeesResponse != null && eventbriteTicketNamesAllowedInRaffle.ToLower().Contains(result.ticket_class_name.ToLower()) == false)
+                {
+                    // Found but ticket not allowed.
+                    result = null;
+                }
+                else
+                {
+                    result = attendeesResponse.attendees.FirstOrDefault(a => a.profile.email.ToLowerInvariant() == attendeeInfo.Email.ToLowerInvariant());
+                }
+
+                return Ok(result);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception);
+            }
         }
 
+        private bool IsValidRequest(LegacyApiCredentialsDTO attendeeInfo)
+        {
+            if (attendeeInfo == null)
+            {
+                return false;
+            }
+
+            var isValidUser = attendeeInfo.User != null && attendeeInfo.User.ToLower() == this.MOBILE_APP_USER;
+            var isValidToken = attendeeInfo.Token != null && attendeeInfo.Token.ToUpper() == this.MOBILE_APP_TOKEN;
+            return isValidUser && isValidToken;
+        }
     }
 }
