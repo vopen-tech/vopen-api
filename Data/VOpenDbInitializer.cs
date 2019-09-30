6ade4c65ad54ce6221f436b3e5948d71e010f951
Nano Vazquez <marianodvazquez@gmail.com>
2019-09-30 17:41:27 -0300

    Update CO date

--- Data/VOpenDbInitializer.cs
+++ Data/VOpenDbInitializer.cs
@@ -1043,8 +1043,8 @@ namespace vopen_api.Data
                     Event = vopenEvent,
                     Details = new EditionDetail[]
                     {
-                       new EditionDetail { Language = Constants.LANGUAGES_SPANISH, Name = "vOpen CO 2020", Date = "26 27 y 28 de Marzo" },
-                       new EditionDetail { Language = Constants.LANGUAGES_ENGLISH, Name = "vOpen CO 2020", Date = "March 26th, 27th and 28th" }
+                       new EditionDetail { Language = Constants.LANGUAGES_SPANISH, Name = "vOpen CO 2020", Date = "26 27 y 28 de Marzo de 2020" },
+                       new EditionDetail { Language = Constants.LANGUAGES_ENGLISH, Name = "vOpen CO 2020", Date = "March 26th, 27th and 28th, 2020" }
                     },
                     LocationName = "Bogotá, Colombia",
                     LocationFullAddress = "Bogotá, Colombia",
